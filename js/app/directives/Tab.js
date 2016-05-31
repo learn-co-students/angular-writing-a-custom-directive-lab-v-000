a8bc898613fcacdc61dfb03ab26ce8450b1b88d1
authorbeard <msnolan@gmail.com>
2016-05-31 13:34:03 -0400

    reverted to two-directive setup; couldn't get components to work

--- js/app/directives/Tab.js
+++ js/app/directives/Tab.js
@@ -1,38 +1,28 @@
-var Tab = {
-  // return {
-    // restrict: 'E',
+function Tab(){
+  return {
+    restrict: 'E',
     transclude: true, 
-    require: {tabsCtrl: '^tabs'},
-    bindings: {
+    require: '^tabs',
+    scope: {
       label: '@'
-    },
-    controller: function(){
-      debugger;
-        this.tab={
-          label: this.label, 
-          selected: false
-        }
-      debugger;
-        this.tabsCtrl.addTab(this.tab)
-
     },
     template: `
         <div class="tabs__content" ng-if="tab.selected">
           <div ng-transclude></div>
         </div>`,
-    // link: function($scope, $element, $attrs, $ctrl){
-    //   $scope.tab={
-    //     label: $scope.label,
-    //     selected: false
-    //   }
-    //   $ctrl.addTab($scope.tab)
-    // }
+    link: function($scope, $element, $attrs, $ctrl){
+      $scope.tab={
+        label: $scope.label,
+        selected: false
+      }
+      $ctrl.addTab($scope.tab)
+    }
 
-  // }
+  }
 
 }
 
 
 angular 
     .module('app')
-    .component('tab', Tab)
+    .directive('tab', Tab)
