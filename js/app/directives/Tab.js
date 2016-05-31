1a0b06681c7917c3bd32c2fe923538a9aee5e0ff
authorbeard <msnolan@gmail.com>
2016-05-31 12:48:39 -0400

    began refactoring to use components

--- js/app/directives/Tab.js
+++ js/app/directives/Tab.js
@@ -1,28 +1,38 @@
-function Tab (){
-  return {
-    restrict: 'E',
-    scope: {
+var Tab = {
+  // return {
+    // restrict: 'E',
+    transclude: true, 
+    require: {tabsCtrl: '^tabs'},
+    bindings: {
       label: '@'
     },
-    require: '^tabs',
-    transclude: true, 
+    controller: function(){
+      debugger;
+        this.tab={
+          label: this.label, 
+          selected: false
+        }
+      debugger;
+        this.tabsCtrl.addTab(this.tab)
+
+    },
     template: `
         <div class="tabs__content" ng-if="tab.selected">
           <div ng-transclude></div>
         </div>`,
-    link: function($scope, $element, $attrs, $ctrl){
-      $scope.tab={
-        label: $scope.label,
-        selected: false
-      }
-      $ctrl.addTab($scope.tab)
-    }
+    // link: function($scope, $element, $attrs, $ctrl){
+    //   $scope.tab={
+    //     label: $scope.label,
+    //     selected: false
+    //   }
+    //   $ctrl.addTab($scope.tab)
+    // }
 
-  }
+  // }
 
 }
 
 
 angular 
     .module('app')
-    .directive('tab', Tab)
+    .component('tab', Tab)
