b1dd47a3437b934fded1a72ad17c32a49563a948
jsgoldb <jgoldb@yahoo.com>
2016-06-03 11:57:43 -0500

    Done.

---
+++ js/app/directives/Tabs.js
@@ -0,0 +1,40 @@
+function tabs() {
+  return {
+    restrict: 'E',
+    scope: {},
+    transclude: true,
+    controller: function () {
+      this.tabs = [];
+      this.addTab = function addTab(tab) {
+        this.tabs.push(tab);
+      };
+      this.selectTab = function selectTab(index) {
+        for (var i = 0; i < this.tabs.length; i++) {
+          this.tabs[i].selected = false;
+        }
+        this.tabs[index].selected = true;
+      };
+    },
+    controllerAs: 'tabs',
+    template: [
+      '<div class="tabs">',
+        '<ul class="tabs__list">',
+          '<li ng-repeat="tab in tabs.tabs">',
+            '<a href="" ng-bind="tab.label" ng-click="tabs.selectTab($index);"></a>',
+          '</li>',
+        '</ul>',
+        '<div class="tabs__content" ng-transclude></div>',
+      '</div>'
+    ].join(''),
+    link: function ($scope, $element, $attrs, $ctrl) {
+      $ctrl.selectTab(0);
+    },
+  };
+}
+
+
+
+
+angular
+  .module('app')
+  .directive('tabs', tabs)
