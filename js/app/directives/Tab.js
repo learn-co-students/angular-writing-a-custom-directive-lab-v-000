50f778cdeac8e1708453cd445a5a3326bbb1db32
Nathaniel Miller <dukeoflaser@gmail.com>
2016-10-12 22:19:11 -0700

    Done.

--- js/app/directives/Tab.js
+++ js/app/directives/Tab.js
@@ -2,14 +2,12 @@ var Tab = {
   require: {tabsCtrl: '^tabs'},
   transclude: true,
   controller: function(){
-    // this.$onInit = function () {
-        // this.tabsCtrl.addTab(this);
-      // };
-    console.log(this);
 
-    this.parent = function() {
-      console.log(this.tabsCtrl);
+    this.$onInit = function(){
+      this.tabsCtrl.addTab(this);
     }
+
+
 
   },
   controllerAs: 'tab',
@@ -17,11 +15,7 @@ var Tab = {
     label: '@'
   },
   template:
-        '<li class="tabs__list">'+
-        '<a href="#">'           +
-          '{{ tab.label }}'      +
-        '</a>'                   +
-        '</li>'
+    '<div ng-transclude ng-show="tab.active"></div>'
 }
 
 angular
--- js/app/directives/Tabs.js
+++ js/app/directives/Tabs.js
@@ -5,16 +5,29 @@ var Tabs = {
 
     this.addTab = function(tab){
       this.tabs.push(tab);
+      this.tabs[0].active = true;
+    }
+
+    this.select = function(tab){
+      this.tabs.forEach(function(tab){
+        tab.active = false;
+      });
+      tab.active = true;
     }
   },
   controllerAs: 'tabs',
   template:
     '<div class="tabs" >' +
-      '<div class="tabs__list">'   +
-        '<ul ng-transclude>'       +
+      '<div class="tabs__list">' +
+        '<ul>'       +
+          '<li class="tabs__list" ng-repeat="tab in tabs.tabs">' +
+            '<a href="#" ng-click="tabs.select(tab)">'           +
+              '{{ tab.label }}'      +
+            '</a>'                   +
+          '</li>' +
         '</ul>' +
       '</div>' +
-    '<div class="tabs__content"></div>'
+    '<div class="tabs__content" ng-transclude></div>'
 }
 
 angular
