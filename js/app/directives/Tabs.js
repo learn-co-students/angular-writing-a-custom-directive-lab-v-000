273fcd3c64fe065e1c5bc5780f94a7d734bd0e0e
ndaniele <n.daniele@gmail.com>
2016-11-07 13:26:47 -0500

    Done.

--- js/app/directives/Tabs.js
+++ js/app/directives/Tabs.js
@@ -1,22 +1,15 @@
-var tabs = {
-  template: [
-      '<div class="tabs">',
-        '<ul class="tabs__list">',
-          '<li ng-repeat="tab in tabs.tabs">',
-            '<a href="" ng-bind="tab.label" ng-click="tabs.selectTab($index);"></a>',
-          '</li>',
-        '</ul>',
-        '<div class="tabs__content" ng-transclude></div>',
-      '</div>'
-    ].join(''),
+function Tabs() {
+  return {
+    restrict: 'E',
+    scope: {},
+    transclude: true,
     controller: function () {
       this.tabs = [];
-
-      this.addTab = function(tab) {
+      this.addTab = function addTab(tab) {
         this.tabs.push(tab);
       };
       this.selectTab = function selectTab(index) {
-        for(var i = 0; i < this.tabs.length; i++) {
+        for (var i = 0; i < this.tabs.length; i++) {
           this.tabs[i].selected = false;
         }
         this.tabs[index].selected = true;
@@ -24,12 +17,21 @@ var tabs = {
     },
     controllerAs: 'tabs',
     link: function ($scope, $element, $attrs, $ctrl) {
-        $ctrl.selectTab($attrs.active || 0);
+      $ctrl.selectTab($attrs.active || 0);
     },
-    
+    template: [
+            '<div class="tabs">',
+              '<ul class="tabs__list">',
+                    '<li ng-repeat="tab in tabs.tabs">',
+                      '<a href="" ng-bind="tab.label" ng-click="tabs.selectTab($index);"></a>',
+                    '</li>',
+                '</ul>',
+              '<div class="tabs__content" ng-transclude></div>',
+            '</div>'
+    ].join('')
   };
+}
 
- 
 angular
-  .module('app', [])
-  .component('tabs', tabs);
+  .module('app')
+  .directive('tabs', Tabs);
