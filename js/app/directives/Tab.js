154f52ae1e0c59c72963896586edc8dfa192dd51
jbok4 <jbok4@aol.com>
2016-12-19 20:02:22 -0500

    complete using directives

--- js/app/directives/Tab.js
+++ js/app/directives/Tab.js
@@ -1,18 +1,25 @@
-var Tab =  {
-  bindings: {
-    label: '='
-  },
-  require: { parent: '^tabs'},
-  transclude: true,
-    template: [
-      '<div class="tabs__content" ng-if="tab.selected">',
-        '<div ng-transclude></div>',
-      '</div>'
-    ].join(''),
-
-
-  };
+function tab() {
+  return {
+    restrict: 'E',
+    scope: {
+      label: '@'
+    },
+    require: '^tabs',
+    transclude: true,
+    template: `
+      <div class="tabs__content" ng-if="tab.selected">
+        <div ng-transclude></div>
+      </div>`,
+    link: function ($scope, $element, $attrs, $ctrl) {
+      $scope.tab = {
+        label: $scope.label,
+        selected: false
+      };
+      $ctrl.addTab($scope.tab);
+    }
+  }
+}
 
 angular
   .module('app')
-  .component('tab', Tab);
+  .directive('tab', tab);
