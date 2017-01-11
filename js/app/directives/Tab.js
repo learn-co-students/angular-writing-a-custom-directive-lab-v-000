5269e60b9f505ba563adf94a22c4076a4e18c3b6
tpstar <hanlee12@gmail.com>
2017-01-10 22:13:06 -0500

    done

--- js/app/directives/Tab.js
+++ js/app/directives/Tab.js
@@ -1,29 +1,31 @@
-var tab = {
-    bindings: {
-    label: '@'
-  },
-  require: {
-  	parent: '^tabs'
-  },
-  transclude: true,
-  template: `
-    <div class="tabs__content" ng-if="$ctrl.tab.selected">
-      <div ng-transclude></div>
-    </div>
-  `,
-  controller: function () {
-    // this.$onInit = function () {
-    	this.tab = {
-        label: this.label,
+function tab() {
+  return {
+    restrict: 'E', // retrict to element, which means this directive 'tab' will be bound to <tab></tab> in index.html
+    scope: {
+      label: '@' //scope is bound to string value of DOM attribute label (label = "Tab 1")
+    },
+    require: '^tabs', //use require to inherit methods from the parent Directive, tabs, $ctrl in the link is
+                      //the parent tabs controller
+    transclude: true, //to insert trascluded DOM element to line 12 <div ng-trasclude></div>
+    template: `
+      <div class="tabs__content" ng-if="tab.selected">
+        <div ng-transclude></div>
+      </div>
+    `, // ng-transclude calls Tab # contents! DOM text and insert that into the div
+
+    // Directives that want to modify the DOM typically use the link option to register DOM listeners as well as update the DOM
+    link: function ($scope, $element, $attrs, $ctrl) {
+      $scope.tab = {
+        label: $scope.label,
         selected: false
       };
-      console.log(this.tabs);
-      this.parent.addTab(this.tab);
-    // };
-  }
-};
+      // $scope.tab = {label: "Tab 1", selected: false}
+      $ctrl.addTab($scope.tab);
+    }
+  };
+}
 
 angular
   .module('app', [])
-  .component('tab', tab)
+  .directive('tab', tab)
   .directive('tabs', tabs);
