b1f447baddbed55af2ea716e1e13da64c885f4af
lynch16 <will.lynch91@gmail.com>
2017-02-03 10:52:45 -0500

    Done.

---
+++ js/app/directives/Tab.js
@@ -0,0 +1,25 @@
+function Tab() {
+  return {
+    transclude: true,
+    scope: {
+      label: "@"
+    },
+    require: '^tabs',
+    template: [
+      '<div class="tabs__content" ng-if="tab.selected">',
+            '<div ng-transclude></div>',
+        '</div>'
+    ].join(''),
+    link: function(scope, elem, attr, ctrl){
+      scope.tab = {
+        label: scope.label,
+        selected: false
+      };
+      ctrl.addTab(scope.tab)
+    }
+  }
+}
+
+angular
+  .module('app')
+  .directive('tab', Tab)
